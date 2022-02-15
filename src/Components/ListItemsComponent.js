d79c8edfa908bb153a62f7adfdcb81d6e4a1a1a6
Bohlken,Joep J <j.bohlken@student.fontys.nl>
2022-02-15 14:32:12 +0100

    puuush

---
+++ src/Components/ListItemsComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+
+function ListItemsComponent() {
+    return (
+        <div></div>
+    );
+  }
+  
+  export default ListItemsComponent;
