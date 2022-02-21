38a5cd0c4fd62f54c6c8fb946464a2d698347867
Bohlken,Joep J <joepbohlken@gmail.com>
2022-02-21 22:45:31 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import Container from "@mui/material/Container";
+
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
+
 import Checkbox from "@mui/material/Checkbox";
+
 import IconButton from "@mui/material/IconButton";
-import CommentIcon from "@mui/icons-material/Comment";
+
 import EditIcon from "@mui/icons-material/Edit";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+
+import TextField from "@mui/material/TextField";
+import { SettingsInputAntenna } from "@mui/icons-material";
 
 function App() {
+  const [task, setTask] = useState([]);
+  const [input, setInput] = useState([]);
+
+  const saveInput = (e) => {
+    setInput(e.target.value);
+  };
+
+  const addTask = () => {
+    setTask((prevState) => [...task, prevState]);
+  };
+
   const [checked, setChecked] = React.useState([0]);
 
   const handleToggle = (value) => {
@@ -26,16 +43,26 @@ function App() {
 
     setChecked(newChecked);
   };
+
   return (
     <div
       style={{
         margin: "auto",
         width: "50%",
-        border: "3px solid green",
+        border: "3px",
+        textAlign: "center",
         padding: "10px",
       }}
     >
-      <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+      <List
+        sx={{
+          width: "100%",
+          maxWidth: 360,
+          bgcolor: "#FAF9F6",
+          margin: "auto",
+          borderRadius: "8px",
+        }}
+      >
         {[0, 1, 2, 3].map((value) => {
           const labelId = `checkbox-list-label-${value}`;
 
@@ -43,7 +70,7 @@ function App() {
             <ListItem
               key={value}
               secondaryAction={
-                <IconButton edge="end" aria-label="comments">
+                <IconButton edge="end" aria-label="edit">
                   <EditIcon />
                 </IconButton>
               }
@@ -63,11 +90,26 @@ function App() {
                     inputProps={{ "aria-labelledby": labelId }}
                   />
                 </ListItemIcon>
-                <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+                <ListItemText id={labelId} primary={task} />
               </ListItemButton>
             </ListItem>
           );
         })}
+        <ListItem
+          secondaryAction={
+            <IconButton onClick={addTask} edge="end" aria-label="add">
+              <AddCircleIcon />
+            </IconButton>
+          }
+        >
+          <TextField
+            label="Add task name..."
+            id="outlined-size-small"
+            defaultValue=""
+            size="small"
+            onChange={saveInput}
+          />
+        </ListItem>
       </List>
     </div>
   );
